079f8ef6844940300898cc83fb4be53e039c92dc
PinniboinaRajesh <141126025+PinniboinaRajesh@users.noreply.github.com>
2024-03-15 16:23:45 +0530

    otp file

---
+++ project/base/templates/registration/otp.py
@@ -0,0 +1,17 @@
+#from twilio.rest import Client
+#import random
+
+
+
+
+#client=Client(account_sid,auth_token)
+
+#message=client.messages.create(
+#
+#	body="{:04d}".format(random.randint(0, 9999)),
+#	from_=twilio_number,
+#	to=target_number
+#)
+#
+#print(message.body)
+
