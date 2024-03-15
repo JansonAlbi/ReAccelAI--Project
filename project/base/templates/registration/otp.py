e68772a00b82cc9745b3d70715b40722a44dddfc
PinniboinaRajesh <141126025+PinniboinaRajesh@users.noreply.github.com>
2024-03-15 16:29:22 +0530

    Update otp.py

--- project/base/templates/registration/otp.py
+++ project/base/templates/registration/otp.py
@@ -1,7 +1,10 @@
 #from twilio.rest import Client
 #import random
 
-
+#account_sid='ACe1a88679f57ef25c3927143035193c10'
+#auth_token='5b4e28cb1ad80a0297d0888328bfbcf4'
+#twilio_number='+17632805129'
+#target_number='+919989548717'
 
 
 #client=Client(account_sid,auth_token)
